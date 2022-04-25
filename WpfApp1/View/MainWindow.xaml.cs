b78bb1648c20698b5eda24fcfeb756eeb0bfe370
holybiter <maksym.b1999@gmail.com>
2022-04-25 02:12:29 +0200

    Last changes

--- WpfApp1/View/MainWindow.xaml.cs
+++ WpfApp1/View/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace WpfApp1.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double actualScrollBarWidth = SystemParameters.VerticalScrollBarWidth + 10;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,18 +30,18 @@ namespace WpfApp1.View
 
         private void ResizeServicesListColumns(object sender, SizeChangedEventArgs args)
         {
-            switch (listViewServices.ActualWidth)
+            switch (args.NewSize.Width)
             {
-                case <= 900 and > 400:
+                case <= 1100 and > 700:
                     AdaptWindowLarge();
                     break;
-                case <= 400 and > 200:
+                case <= 700 and > 500:
                     AdaptWindowMiddle();
                     break;
-                case <= 200 and > 100:
+                case <= 500 and > 400:
                     AdaptWindowLittle();
                     break;
-                case <= 100:
+                case <= 400:
                     AdaptWindowMinimum();
                     break;
                 default:
@@ -51,68 +53,110 @@ namespace WpfApp1.View
 
         public void AdaptWindowMaximum()
         {
-            double newColumnWidth = listViewServices.ActualWidth / gridViewServices.Columns.Count;
-            foreach (var column in gridViewServices.Columns)
+            while (mainGrid.RowDefinitions.Count > 1)
             {
-                column.Width = newColumnWidth;
+                mainGrid.RowDefinitions.RemoveAt(mainGrid.RowDefinitions.Count - 1);
             }
+
+            listViewServices.SetValue(Grid.ColumnProperty, 0);
+            listViewServices.SetValue(Grid.RowProperty, 0);
+
+            mainGrid.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Star);
+            mainGrid.ColumnDefinitions[0].Width = new GridLength(3, GridUnitType.Star);
+            mainGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Auto);
+
+            double newColumnWidth = (listViewServices.ActualWidth - actualScrollBarWidth) / gridViewServices.Columns.Count;
+            if (newColumnWidth > 0)
+                foreach (var column in gridViewServices.Columns)
+                {
+                    column.Width = newColumnWidth;
+                }
         }
 
         public void AdaptWindowLarge()
         {
+            while (mainGrid.RowDefinitions.Count > 1)
+            {
+                mainGrid.RowDefinitions.RemoveAt(mainGrid.RowDefinitions.Count - 1);
+            }
+
             listViewServices.SetValue(Grid.ColumnProperty, 0);
+            listViewServices.SetValue(Grid.RowProperty, 0);
+
+            mainGrid.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Star);
             mainGrid.ColumnDefinitions[0].Width = new GridLength(3, GridUnitType.Star);
             mainGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Auto);
-            double newColumnWidth = listViewServices.ActualWidth / (gridViewServices.Columns.Count - 1);
-            foreach (var column in gridViewServices.Columns)
-            {
-                if (column.Header.ToString() != "Account")
+
+            double newColumnWidth = (listViewServices.ActualWidth - actualScrollBarWidth) / (gridViewServices.Columns.Count - 1);
+            if (newColumnWidth > 0)
+                foreach (var column in gridViewServices.Columns)
                 {
-                    column.Width = newColumnWidth;
+                    if (column.Header.ToString() != "Account")
+                    {
+                        column.Width = newColumnWidth;
+                    }
+                    else
+                    {
+                        column.Width = 0;
+                    }
                 }
-                else
-                {
-                    column.Width = 0;
-                }
-            }
         }
 
         public void AdaptWindowMiddle()
         {
+            while (mainGrid.RowDefinitions.Count > 1)
+            {
+                mainGrid.RowDefinitions.RemoveAt(mainGrid.RowDefinitions.Count - 1);
+            }
+
             listViewServices.SetValue(Grid.ColumnProperty, 0);
+            listViewServices.SetValue(Grid.RowProperty, 0);
+
+            mainGrid.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Star);
             mainGrid.ColumnDefinitions[0].Width = new GridLength(3, GridUnitType.Star);
             mainGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Auto);
-            double newColumnWidth = listViewServices.ActualWidth / (gridViewServices.Columns.Count - 2);
-            foreach (var column in gridViewServices.Columns)
-            {
-                if (column.Header.ToString() != "Account" && column.Header.ToString() != "DisplayName")
+
+            double newColumnWidth = (listViewServices.ActualWidth - actualScrollBarWidth) / (gridViewServices.Columns.Count - 2);
+            if (newColumnWidth > 0)
+                foreach (var column in gridViewServices.Columns)
                 {
-                    column.Width = newColumnWidth;
+                    if (column.Header.ToString() != "Account" && column.Header.ToString() != "DisplayName")
+                    {
+                        column.Width = newColumnWidth;
+                    }
+                    else
+                    {
+                        column.Width = 0;
+                    }
                 }
-                else
-                {
-                    column.Width = 0;
-                }
-            }
         }
 
         public void AdaptWindowLittle()
         {
+            while (mainGrid.RowDefinitions.Count > 1)
+            {
+                mainGrid.RowDefinitions.RemoveAt(mainGrid.RowDefinitions.Count - 1);
+            }
+
             listViewServices.SetValue(Grid.ColumnProperty, 0);
+            listViewServices.SetValue(Grid.RowProperty, 0);
+
+            mainGrid.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Star);
             mainGrid.ColumnDefinitions[0].Width = new GridLength(3, GridUnitType.Star);
             mainGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Auto);
-            double newColumnWidth = listViewServices.ActualWidth / (gridViewServices.Columns.Count - 3);
-            foreach (var column in gridViewServices.Columns)
-            {
-                if (column.Header.ToString() != "Account" && column.Header.ToString() != "DisplayName" && column.Header.ToString() != "Status")
+
+            double newColumnWidth = (listViewServices.ActualWidth - actualScrollBarWidth) / (gridViewServices.Columns.Count - 3);
+            if (newColumnWidth > 0) foreach (var column in gridViewServices.Columns)
                 {
-                    column.Width = newColumnWidth;
+                    if (column.Header.ToString() != "Account" && column.Header.ToString() != "DisplayName" && column.Header.ToString() != "Status")
+                    {
+                        column.Width = newColumnWidth;
+                    }
+                    else
+                    {
+                        column.Width = 0;
+                    }
                 }
-                else
-                {
-                    column.Width = 0;
-                }
-            }
         }
 
         public void AdaptWindowMinimum()
@@ -120,25 +164,29 @@ namespace WpfApp1.View
             mainGrid.ColumnDefinitions[0].Width = new GridLength(0, GridUnitType.Star);
             mainGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
 
-            mainGrid.RowDefinitions.Add(new RowDefinition());
+            if (mainGrid.RowDefinitions.Count < 2)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+            }
             mainGrid.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Auto);
             mainGrid.RowDefinitions[1].Height = new GridLength(1, GridUnitType.Star);
 
             listViewServices.SetValue(Grid.RowProperty, 1);
             listViewServices.SetValue(Grid.ColumnProperty, 1);
 
-            double newColumnWidth = listViewServices.ActualWidth / (gridViewServices.Columns.Count - 3);
-            foreach (var column in gridViewServices.Columns)
-            {
-                if (column.Header.ToString() != "Account" && column.Header.ToString() != "DisplayName" && column.Header.ToString() != "Status")
+            double newColumnWidth = (listViewServices.ActualWidth - actualScrollBarWidth) / (gridViewServices.Columns.Count - 3);
+            if (newColumnWidth > 0)
+                foreach (var column in gridViewServices.Columns)
                 {
-                    column.Width = newColumnWidth;
+                    if (column.Header.ToString() != "Account" && column.Header.ToString() != "DisplayName" && column.Header.ToString() != "Status")
+                    {
+                        column.Width = newColumnWidth;
+                    }
+                    else
+                    {
+                        column.Width = 0;
+                    }
                 }
-                else
-                {
-                    column.Width = 0;
-                }
-            }
         }
     }
 }
